73d473b49cdf5566791b7fc146ed40803a23b368
fincs <fincs.alt1@gmail.com>
2014-08-26 22:57:06 +0200

    Remove ctrulib gfx.h copypaste

--- source/gfx.h
+++ source/gfx.h
@@ -4,31 +4,6 @@
 #include <3ds/types.h>
 #include <3ds/gfx.h>
 
-typedef enum
-{
-	GFX_TOP = 0,
-	GFX_BOTTOM = 1
-}gfxScreen_t;
-
-typedef enum
-{
-	GFX_LEFT = 0,
-	GFX_RIGHT = 1,
-	// GFX_BOTTOM = 0
-}gfx3dSide_t;
-
-//system stuff
-void gfxInit();
-void gfxExit();
-
-//control stuff
-void gfxSet3D(bool enable);
-void gfxFlushBuffers();
-void gfxSwapBuffers();
-
-//helper stuff
-u8* gfxGetFramebuffer(gfxScreen_t screen, gfx3dSide_t side, u16* width, u16* height);
-
 //rendering stuff
 void gfxDrawSprite(gfxScreen_t screen, gfx3dSide_t side, u8* spriteData, u16 width, u16 height, s16 x, s16 y);
 void gfxDrawDualSprite(u8* spriteData, u16 width, u16 height, s16 x, s16 y);
@@ -39,9 +14,5 @@ void gfxDrawText(gfxScreen_t screen, gfx3dSide_t side, char* str, u16 x, u16 y);
 void gfxFillColor(gfxScreen_t screen, gfx3dSide_t side, u8 rgbColor[3]);
 void gfxDrawRectangle(gfxScreen_t screen, gfx3dSide_t side, u8 rgbColor[3], s16 x, s16 y, u16 width, u16 height);
 void gfxDrawWave(gfxScreen_t screen, gfx3dSide_t side, u8 rgbColor[3], u16 level, u16 amplitude, u16 t, u16 width);
-
-//global variables
-extern u8* topLeftFramebuffers[2];
-extern u8* subFramebuffers[2];
 
 #endif
